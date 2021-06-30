dbfb4d143f25651ee40692e2475724f203d5770a
Pav Rao <pav0107@gmail.com>
2021-06-30 20:45:55 +0100

    added method for docking + unit test for it, and an attr_reader for bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,6 @@
 require 'docking_station'
 
 describe DockingStation do
-  
-  it {is_expected.to respond_to(:release_bike)}
 
   it "gets a bike" do
     docking_station = DockingStation.new
@@ -14,4 +12,20 @@ describe DockingStation do
     bike = docking_station.release_bike
     expect(bike.working?).to eq(true) 
   end
+
+  it {is_expected.to respond_to(:dock).with(1).argument }
+
+  it {is_expected.to respond_to(:bike) }
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
 end
