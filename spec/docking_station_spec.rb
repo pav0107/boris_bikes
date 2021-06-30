8fdc6ff50ccb7532e468673db3f5fdbf86875662
Else Meijerink <elsemeijerink@MA305.lan>
2021-06-30 16:05:27 +0100

    Added functionality to  get bike and check if working through DockingStationv & added tests to cover this

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,17 @@
 require 'docking_station'
 
 describe DockingStation do
+  
   it {is_expected.to respond_to(:release_bike)}
+
+  it "gets a bike" do
+    docking_station = DockingStation.new
+    expect(docking_station.release_bike.class).to eq(Bike)
+  end
+  
+  it "bike is working" do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect(bike.working?).to eq(true) 
+  end
 end
